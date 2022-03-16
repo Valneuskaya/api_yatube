d33ecf537e66e2b703baa7c969cb2fc00b180f4c
Valeri Neuskaya <valerineuskaya@yandex.by>
2022-03-16 21:19:38 +0100

    add api to aps in settings

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,7 +4,7 @@ from django.core.exceptions import PermissionDenied
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
-from serializers import GroupSerializer, PostSerializer, CommentSerializer
+from api.serializers import GroupSerializer, PostSerializer, CommentSerializer
 
 from posts.models import Comment, Group, Post
 
@@ -13,22 +13,22 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def post_create(self, serializer):
+    def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def post_update(self, serializer):
+    def perform_update(self, serializer):
         if self.request.user != serializer.instance.author:
             raise PermissionDenied()
         serializer.save(author=self.request.user, status=status.HTTP_200_OK)
 
-    def post_delete(self, instance):
+    def perform_destroy(self, instance):
         if self.request.user != instance.author:
             raise PermissionDenied()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
@@ -41,13 +41,13 @@ class CommentViewSet(viewsets.ModelViewSet):
         post = get_object_or_404(Post, id=post_id)
         return Comment.objects.filter(post=post)
 
-    def comment_create(self, serializer):
+    def perform_create(self, serializer):
         post_id = self.kwargs.get("post_id")
         serializer.save(
             author=self.request.user, post=get_object_or_404(Post, id=post_id)
         )
 
-    def comment_update(self, serializer):
+    def perform_update(self, serializer):
         post_id = self.kwargs.get("post_id")
         if self.request.user != serializer.instance.author:
             raise PermissionDenied()
@@ -57,7 +57,7 @@ class CommentViewSet(viewsets.ModelViewSet):
             status=status.HTTP_200_OK,
         )
 
-    def comment_delete(self, instance):
+    def perform_destroy(self, instance):
         if self.request.user != instance.author:
             raise PermissionDenied()
         instance.delete()
